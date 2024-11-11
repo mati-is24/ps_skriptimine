dc906fa4135231e969ec507ad4a9be675563905a
Mati Lillemaa <mati.lillemaa@voco.ee>
2024-11-11 14:39:04 +0200

    lahendatud += käsuga array

---
+++ ül1.ps1
@@ -0,0 +1,14 @@
+﻿# define data arrays
+$Array1 = @(1, 2, 3)
+$Array2 = @(4, 5, 6)
+
+#define empty result array
+$array3 = @()
+
+#add new values to empty arrat
+$Array3 += $Array1[0] + $Array2[0]
+$Array3 += $Array1[1] + $Array2[1]
+$Array3 += $Array1[2] + $Array2[2]
+
+#output result array
+$array3

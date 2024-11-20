658a55d3b3c0d9486901f95449ed48787f46027b
Mati Lillemaa <mati.lillemaa@voco.ee>
2024-11-20 11:37:19 +0200

    Changes to be committed: 	new file:   pr6.ps1

---
+++ pr6.ps1
@@ -0,0 +1,16 @@
+﻿$data = Import-Csv C:\Users\mati\ps_skriptimine\data.csv
+
+$schooldata = @()
+ForEach($persondata in $data){
+
+    $persondata.Age = [int]$persondata.Age
+    $school = ""
+
+    if($persondata.Age -ge 4 -and $persondata.Age -le 10){
+        $school = "Junior"
+    } elseif($persondata.Age -ge 11 -and $persondata.Age -le 17){
+    $school = "Senior"
+    }
+
+    $schooldata += [PSCustomObject]@{'Name'=$persondata.Name; 'School'=$school}
+ }

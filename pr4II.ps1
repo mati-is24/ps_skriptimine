98f46a38a486ed67a5dbf4f05c066319de6e8471
Mati Lillemaa <mati.lillemaa@voco.ee>
2024-11-20 10:46:39 +0200

    Changes to be committed: 	new file:   pr4II.ps1

---
+++ pr4II.ps1
@@ -0,0 +1,10 @@
+﻿$countries = @( 'India', 'Australia', 'China')
+$capitals = @('New Delhi', 'Canberra', 'Beijing')
+
+Write-Host "Select country" -ForegroundColor Yellow
+for($i =0; $i -lt $countries.count; $i++){
+    Write-Host ($i+1)":" $countries[$i] -ForegroundColor Cyan
+}
+
+$selectedcountry = Read-Host "Please select country"
+Write-Host $countries[$selectedcountry-1]"'s capital is"$capitals[$selectedcountry-1] -ForegroundColor Green

986b202ecf704b8c33c926173a501ad5393f9f48
Mati Lillemaa <mati.lillemaa@voco.ee>
2024-11-20 10:46:53 +0200

    Changes to be committed: 	new file:   pr4.ps1

---
+++ pr4.ps1
@@ -0,0 +1,16 @@
+﻿$number1 = Read-Host "insert first number"
+$number2 = Read-Host "insert second number"
+
+$biggerNumber = 0
+
+if($number1 -gt $number2){
+    $biggerNumber = $number1
+    } elseif ($number2 -gt $number1){
+    $biggerNumber = $number2
+    } else {
+        $biggerNumber = $number1
+        Write-Host "Numbers are equal, the biggest is: $biggerNumber"
+        exit 0
+    }
+
+    Write-Host "The higher number is : $biggerNumber"

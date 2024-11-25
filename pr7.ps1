4d075320ad36da57c05d6b417610991c8c7822f0
Mati Lillemaa <mati.lillemaa@voco.ee>
2024-11-25 12:13:41 +0200

    Changes to be committed: 	new file:   pr7.ps1

---
+++ pr7.ps1
@@ -0,0 +1,8 @@
+﻿$groups = @('Red', 'Green', 'Yellow', 'Blue')
+$studentGroups = @()
+
+for($i = 1; $i -le 20; $i++){
+    $group = $groups | Get-Random
+    $studentGroups += [PsCustomObject]@{'RollNumber'=$i; 'Group' =$group}
+    }
+$studentGroups

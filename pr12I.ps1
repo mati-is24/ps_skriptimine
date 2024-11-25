d21ea777881910ea1cba98909469731d0686bb72
Mati Lillemaa <mati.lillemaa@voco.ee>
2024-11-25 14:55:47 +0200

    funktsioonid ja parameetrid

---
+++ pr12I.ps1
@@ -0,0 +1,23 @@
+﻿function countstart{
+    $total = (Get-Service | where{$_.Status -eq 'running'}).Count
+    Write-Host "Total services in running state = $($total)"
+}
+
+function countstop{
+    $total = (Get-Service | where{$_.Status -eq 'stopped'}).Count
+    Write-Host "Total services in stopped state = $($total)"
+}
+
+function countservice{
+    param(
+        [string]$status
+    )
+    $total = (Get-Service | where{$_.Status -eq $status}).Count
+    Write-Host "Total services in $($status.ToLower()) state = $($total)"
+}
+
+countstart
+countstop
+
+countservice -status 'running'
+countservice('Stopped')

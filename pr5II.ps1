d6ed3dd40861cf169f26678da1a2c649689a77cc
Mati Lillemaa <mati.lillemaa@voco.ee>
2024-11-25 14:02:44 +0200

    Changes to be committed: 	new file:   pr10.ps1 	new file:   pr5I.ps1 	new file:   pr5II.ps1

---
+++ pr5II.ps1
@@ -0,0 +1,11 @@
+﻿Get-ChildItem -Path C:\Users\mati\temp\test
+
+Write-Host
+
+$result = Get-ChildItem C:\Users\mati\temp\test | where{$_.Name -like '*.csv'} | Select Name, length
+    $result.Length/1Kb
+     $result.Length/1MB
+
+     Write-Host "filename: $($result.Name)"
+     Write-Host "Size in KB $($result.Length/1024)"
+     Write-Host "Size in MB $($result.Length/1MB)"
